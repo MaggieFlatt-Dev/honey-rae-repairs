42f1457cd5c50bb8c85c397991a7e74362771b50
Maggie Flatt <maggieflatt19@gmail.com>
2026-01-09 10:30:59 -0600

    created EmployeeList.jsx and Employees.css, all working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CustomerList } from "./components/customers/CustomerList";
 import { TicketList } from "./components/tickets/TicketList";
+import { EmployeeList } from "./components/employees/EmployeeList";
 
 export const App = () => {
   return (
     <>
       {/* <TicketList />  */}
-      <CustomerList />
+      {/* <CustomerList /> */}
+      <EmployeeList />
     </>
   );
 };
